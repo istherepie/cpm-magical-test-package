f0633f2c7474e92b4f67c85a7a4492332a2a4c43
Steffen Park <dev@istherepie.com>
2024-08-10 14:47:28 +0000

    Setup project

---
+++ src/magic.c
@@ -0,0 +1,5 @@
+#include "magic.h"
+
+int add_with_magic(int a, int b) {
+    return a + b + MAGIC_NUMBER;
+}
---
+++ src/magic.h
@@ -0,0 +1,7 @@
+#ifndef ISTHEREPIE_MAGIC_H
+#define ISTHEREPIE_MAGIC_H
+#define MAGIC_NUMBER 3
+
+int add_with_magic(int a, int b);
+
+#endif
